09ac5e0228312d2025e62582fcb104e91dad7d91
Babakulyyev <envader001@gmail.com>
2022-09-03 14:28:29 +0500

    03.09

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import(
   "github.com/atadzan/todo-app"
   "github.com/atadzan/todo-app/pkg/handler"
+  "github.com/atadzan/todo-app/pkg/repository"
+  "github.com/atadzan/todo-app/pkg/service"
   "log"
 )
 
 func main(){
-  handlers := new(handler.Handler)
+  repos := repository.NewRepository()
+  services := service.NewService(repos)
+  handlers := handler.NewHandler(services)
+
   srv := new(todo.Server)
   if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
     log.Fatalf("error occured while running http server: %s", err.Error())
