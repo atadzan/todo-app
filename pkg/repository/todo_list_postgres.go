324442e252718db7a3e1045e976c9b1254f7fce5
Babakulyyev <envader001@gmail.com>
2022-09-09 14:02:38 +0500

    Delete&Update

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -2,8 +2,10 @@ package repository
 
 import(
   "fmt"
+  "strings"
   "github.com/atadzan/todo-app"
   "github.com/jmoiron/sqlx"
+  "github.com/sirupsen/logrus"
 )
 
 type TodoListPostgres struct {
@@ -19,7 +21,6 @@ func (r *TodoListPostgres) Create(userId int, list todo.TodoList) (int, error){
   if err != nil {
     return 0, err
   }
-
   var id int
   createListQuery := fmt.Sprintf("INSERT INTO %s (title, description) VALUES (%1, %2) RETURNING id", todoListsTable)
   row := tx.QueryRow(createListQuery, list.Title, list.Description)
@@ -27,7 +28,6 @@ func (r *TodoListPostgres) Create(userId int, list todo.TodoList) (int, error){
     tx.Rollback()
     return 0, err
   }
-
   createUsersListQuery := fmt.Sprintf("INSERT INTO %s (user_id, list_id) VALUES (%1, %2)", usersListsTable)
   _, err = tx.Exec(createUsersListQuery, userId, id)
   if err != nil{
@@ -39,9 +39,50 @@ func (r *TodoListPostgres) Create(userId int, list todo.TodoList) (int, error){
 
 func (r *TodoListPostgres) GetAll(userId int) ([]todo.TodoList, error) {
   var lists []todo.TodoList
-  query := fmt.Sprintf("SELECT * FROM %s tl INNER JOIN %s ul on tl.id = ul.list_id WHERE ul.user_id = $1",
+  query := fmt.Sprintf("SELECT tl.id, tl.title, tl.description FROM %s tl INNER JOIN %s ul on tl.id = ul.list_id WHERE ul.user_id = $1",
             todoListsTable, usersListsTable)
   err := r.db.Select(&lists, query, userId)
-
   return lists, err
+}
+
+func (r *TodoListPostgres) GetById(userId, listId int) (todo.TodoList, error) {
+  var list todo.TodoList
+  query := fmt.Sprintf("SELECT tl.id, tl.title, tl.description FROM %s tl INNER JOIN %s ul on tl.id = ul.list_id WHERE ul.user_id = $1 AND ul.list_id=$2", todoListsTable, usersListsTable)
+  err := r.db.Get(&list, query, userId, listId)
+  return list, err
+}
+
+func (r *TodoListPostgres) Delete(userId, listId int) error {
+  query := fmt.Sprintf("DELETE FROM %s tl USING %s ul WHERE tl.id = ul.list_id AND ul.user_id=$1 AND ul.list_id=%$2",
+                        todoListsTable, usersListsTable)
+  _, err := r.db.Exec(query, userId, listId)
+  return err
+}
+
+func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput) error {
+  setValues := make([]string, 0)
+  args := make([]interface{}, 0)
+  argId := 1
+
+  if input.Title != nil {
+    setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+    args = append(args, *input.Title)
+    argId++
+  }
+
+  if input.Description != nil {
+    setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+    args = append(args, *input.Description)
+    argId++
+  }
+
+  setQuery := strings.Join(setValues, ", ")
+  query := fmt.Sprintf("UPDATE %s tl SET %s FROM %s ul WHERE tl.id - ul.list_id AND ul.list_id=$%d AND ul.user_id=$%d",
+    todoListsTable, setQuery, usersListsTable, argId, argId+1)
+  args = append(args, listId, userId)
+  logrus.Debugf("updateQuery: %s", query)
+  logrus.Debugf("args: %s", args)
+
+  _, err := r.db.Exec(query, args...)
+  return err
 }
